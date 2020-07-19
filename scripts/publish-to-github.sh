0bf9b4dd88033fa950a301279c0e8178793bbbb5
Septs <github@septs.pw>
2020-07-19 09:57:44 +0800

    automate publish to github package

--- scripts/publish-to-github.sh
+++ scripts/publish-to-github.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo "@dimensiondev:registry=https://npm.pkg.github.com/DimensionDev" > "$HOME/.npmrc"
-
 COMMIT_HSAH=$(git rev-parse --short HEAD)
 npm --no-git-tag-version version "0.0.0-$COMMIT_HSAH"
 npm publish --tag unstable
